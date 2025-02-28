3aa53abb5e57a7603880ab562e21e5338fb03d5e
nahmad0 <nahmad0614@gmal.com>
2025-02-27 23:19:31 -0500

    Heatmap
    Heatmpa for project 1  add with a botton to turn the server on

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-/* script */
+const toggleBtn = document.querySelector('.toggleBtn');
+const contentArea = document.getElementById('contentArea');
+let isVisible = false;
+
+toggleBtn.addEventListener('click', function() {
+    if (isVisible) {
+        contentArea.innerHTML = ''; // Hide the heatmap
+        isVisible = false;
+    } else {
+        contentArea.innerHTML = `
+            <iframe src="https://nahmad0.shinyapps.io/heatmap/" width="100%" height="600" frameborder="0" onload ="<h1>Loading</h1>"></iframe>
+        `; // Show the heatmap
+        isVisible = true;
+    }
+});
